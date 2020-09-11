61d99ae5f855fe7b97dd8318ed4bfafa50309f17
Ramzi <ramzibelbahri@gmail.com>
2020-09-11 15:18:51 -0400

    Fixed mouseup outside canvas behaviour

--- client/src/app/services/tools/pencil-service.ts
+++ client/src/app/services/tools/pencil-service.ts
@@ -13,9 +13,14 @@ import { MouseButton } from '@app/shared/enum';
 })
 export class PencilService extends Tool {
     private pathData: Vec2[];
+    // Todo: Attributs globaux
+    // private color: string;
+    // private opacity: number;
+    private thickness: number;
 
     constructor(drawingService: DrawingService) {
         super(drawingService);
+        this.thickness = 5; // Remplacer par un observable
         this.clearPath();
     }
 
@@ -23,7 +28,6 @@ export class PencilService extends Tool {
         this.mouseDown = event.button === MouseButton.Left;
         if (this.mouseDown) {
             this.clearPath();
-
             this.mouseDownCoord = this.getPositionFromMouse(event);
             this.pathData.push(this.mouseDownCoord);
         }
@@ -40,21 +44,26 @@ export class PencilService extends Tool {
     }
 
     onMouseMove(event: MouseEvent): void {
+        this.mouseDown = event.buttons === 1; // MouseButton.Left;
         if (this.mouseDown) {
             const mousePosition = this.getPositionFromMouse(event);
             this.pathData.push(mousePosition);
-
             // On dessine sur le canvas de prévisualisation et on l'efface à chaque déplacement de la souris
             this.drawingService.clearCanvas(this.drawingService.previewCtx);
             this.drawLine(this.drawingService.previewCtx, this.pathData);
         }
+        this.drawLine(this.drawingService.baseCtx, this.pathData);
     }
 
     private drawLine(ctx: CanvasRenderingContext2D, path: Vec2[]): void {
         ctx.beginPath();
+        ctx.lineCap = 'round';
+        ctx.lineWidth = this.thickness;
         for (const point of path) {
-            ctx.lineTo(point.x, point.y);
+            if (point.x !== this.drawingService.canvas.height || (0 && point.y !== this.drawingService.canvas.width) || 0)
+                ctx.lineTo(point.x, point.y);
         }
+        ctx.strokeStyle = `rgb(${155},${55},${255})`;
         ctx.stroke();
     }
 
