ca51269acaedc4363d8ab00be988f3e43ab7644e
TristanPhilis <46928560+TristanPhilis@users.noreply.github.com>
2020-09-24 21:05:03 -0400

    created eraser-serice and it's spec file
    Signed-Off-By: Tristan Philis <tristan.philis@polymtl.ca>

---
+++ client/src/app/services/tools/eraser-service.ts
@@ -0,0 +1,82 @@
+import { Injectable } from '@angular/core';
+import { Tool } from '@app/classes/tool';
+import { Vec2 } from '@app/classes/vec2';
+import { MouseButton } from '@app/enum';
+import { DrawingService } from '@app/services/drawing/drawing.service';
+
+// Ceci est une implémentation de base de l'outil Crayon pour aider à débuter le projet
+// L'implémentation ici ne couvre pas tous les critères d'accepetation du projet
+// Vous êtes encouragés de modifier et compléter le code.
+// N'oubliez pas de regarder les tests dans le fichier spec.ts aussi!
+@Injectable({
+    providedIn: 'root',
+})
+export class EraserService extends Tool {
+    private pathData: Vec2[];
+    // Todo: Attributs globaux
+    // private color: string;
+    // private opacity: number;
+    private thickness: number;
+
+    constructor(drawingService: DrawingService) {
+        super(drawingService);
+        this.thickness = 5; // Remplacer par un observable
+        this.clearPath();
+    }
+
+    onMouseDown(event: MouseEvent): void {
+        this.mouseDown = event.buttons === MouseButton.Left;
+        if (this.mouseDown) {
+            this.clearPath();
+            this.mouseDownCoord = this.getPositionFromMouse(event);
+            this.pathData.push(this.mouseDownCoord);
+        }
+    }
+
+    onMouseUp(event: MouseEvent): void {
+        if (this.mouseDown) {
+            const mousePosition = this.getPositionFromMouse(event);
+            this.pathData.push(mousePosition);
+            this.drawLine(this.drawingService.baseCtx, this.pathData);
+        }
+        this.mouseDown = false;
+        this.clearPath();
+    }
+
+    onMouseMove(event: MouseEvent): void {
+        if (this.mouseDown && event.buttons === MouseButton.Left) {
+            const mousePosition = this.getPositionFromMouse(event);
+            this.pathData.push(mousePosition);
+            // On dessine sur le canvas de prévisualisation et on l'efface à chaque déplacement de la souris
+            this.drawingService.clearCanvas(this.drawingService.previewCtx);
+            this.eraserLine(this.drawingService.previewCtx, this.pathData);
+        }
+        if (this.mouseDown && !(event.buttons === MouseButton.Left)) {
+            this.eraserLine(this.drawingService.baseCtx, this.pathData);
+        }
+    }
+
+    private eraserLine(ctx: CanvasRenderingContext2D, path: Vec2[]): void {
+        ctx.beginPath();
+        ctx.globalCompositeOperation = 'destination-out';
+        ctx.lineCap = 'round';
+        ctx.lineWidth = this.thickness;
+        for (const point of path) {
+            // const leftBorder = point.x === this.drawingService.canvas.height;
+            // const rightBorder = point.y === this.drawingService.canvas.width;
+            // console.log(this.drawingService.canvas);
+            // if (!leftBorder && !rightBorder) {
+            ctx.lineTo(point.x, point.y);
+            // }
+        }
+        // ctx.strokeStyle = `rgba(${0},${0},${0},${0.05})`;
+        ctx.stroke();
+        for (const point of path) {
+            console.log(ctx.getImageData(point.x, point.y, 1, 1).data[3]);
+        }
+    }
+
+    private clearPath(): void {
+        this.pathData = [];
+    }
+}
