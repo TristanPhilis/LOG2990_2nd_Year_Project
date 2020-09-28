e38436f337c6563f7e4b874a07780d528fd218ad
Romain Saez <romain.saez@polymtl.ca>
2020-09-28 18:09:21 -0400

    draw line

--- client/src/app/services/tools/line-service.ts
+++ client/src/app/services/tools/line-service.ts
@@ -9,6 +9,7 @@ import { DrawingService } from '@app/services/drawing/drawing.service';
 export class LineService extends Tool {
     private pathData: Vec2[];
     private angle: number;
+    private currentPoint: Vec2;
 
     constructor(drawingService: DrawingService) {
         super(drawingService);
@@ -17,12 +18,13 @@ export class LineService extends Tool {
 
     onMouseDown(event: MouseEvent): void {
         if (this.mouseDown) {
+            this.drawingService.previewCtx.beginPath();
+            this.currentPoint = { x: event.offsetX, y: event.offsetY };
+            this.drawingService.previewCtx.stroke();
             this.drawLine(this.drawingService.baseCtx, event);
-            this.clearPath();
-            this.mouseDownCoord = this.getPositionFromMouse(event);
             this.pathData.push(this.mouseDownCoord);
+            console.log(this.pathData, 'DOWN');
         }
-        this.mouseDown = true;
     }
 
     onMouseUp(event: MouseEvent): void {
@@ -40,9 +42,10 @@ export class LineService extends Tool {
             this.drawingService.clearCanvas(this.drawingService.previewCtx);
             this.drawLine(this.drawingService.previewCtx, event);
         }
+        this.mouseDown = true;
     }
 
-    calculateAngle(event: MouseEvent): number {
+    calculateAngle(): number {
         const size = this.pathData.length;
         if (size) {
             const xRadius = Math.abs(this.mouseDownCoord.x - this.pathData[0].x);
@@ -55,13 +58,13 @@ export class LineService extends Tool {
 
     private drawLine(ctx: CanvasRenderingContext2D, event: MouseEvent): void {
         ctx.beginPath();
-        const angle = this.calculateAngle(event);
-        const mouseCoord = this.getPositionFromMouse(event);
+        const angle = this.calculateAngle();
+        const point = this.pathData.pop();
 
         if (event.shiftKey) {
             if (angle < Math.PI / 8) {
                 const x1 = this.pathData[0].x;
-                const x2 = mouseCoord.x;
+                const x2 = this.mouseDownCoord.x;
                 const y1 = this.pathData[0].y;
 
                 ctx.lineTo(x1, y1);
@@ -69,35 +72,33 @@ export class LineService extends Tool {
             } else if (angle > (3 * Math.PI) / 8) {
                 const x1 = this.pathData[0].x;
                 const y1 = this.pathData[0].y;
-                const y2 = mouseCoord.y;
+                const y2 = this.mouseDownCoord.y;
 
                 ctx.lineTo(x1, y1);
                 ctx.lineTo(x1, y2);
             } else {
                 const x1 = this.pathData[0].x;
-                const x2 = mouseCoord.x;
+                const x2 = this.mouseDownCoord.x;
                 const y1 = this.pathData[0].y;
-                const deltaX = this.pathData[0].x - mouseCoord.x;
-                const deltaY = this.pathData[0].y - mouseCoord.y;
+                const deltaX = this.pathData[0].x - this.mouseDownCoord.x;
+                const deltaY = this.pathData[0].y - this.mouseDownCoord.y;
 
                 if ((deltaX > 0 && deltaY < 0) || (deltaX < 0 && deltaY > 0)) {
-                    const y2 = Math.round(deltaX + mouseCoord.y);
+                    const y2 = Math.round(deltaX + this.mouseDownCoord.y);
                     ctx.lineTo(x1, y1);
                     ctx.lineTo(x2, y2);
                 } else {
-                    const y2 = Math.round(-deltaX + mouseCoord.y);
+                    const y2 = Math.round(-deltaX + this.mouseDownCoord.y);
                     ctx.lineTo(x1, y1);
                     ctx.lineTo(x2, y2);
                 }
             }
         } else {
             ctx.lineTo(event.offsetX, event.offsetY);
-            const x1 = this.pathData[0].x;
-            const y1 = this.pathData[0].y;
-            const x2 = this.pathData[1].x;
-            const y2 = this.pathData[1].y;
-            ctx.lineTo(x1, y1);
-            ctx.lineTo(x2, y2);
+            if (point && this.currentPoint) {
+                ctx.lineTo(this.currentPoint.x, this.currentPoint.y);
+                ctx.lineTo(point.x, point.y);
+            }
         }
         ctx.stroke();
     }
