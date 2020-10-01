c1cf6c1117e41f92cc208536b6eee8a41b681d3c
gamor1 <gabriel-1.moreau@polymtl.ca>
2020-10-01 15:30:52 -0400

    added test to get 100 coverage

--- client/src/app/services/tools/line-service.spec.ts
+++ client/src/app/services/tools/line-service.spec.ts
@@ -2,7 +2,7 @@ import { TestBed } from '@angular/core/testing';
 import { canvasTestHelper } from '@app/classes/canvas-test-helper';
 import { Vec2 } from '@app/classes/vec2';
 import { DrawingService } from '@app/services/drawing/drawing.service';
-import { BACKSPACE_KEY, SHIFT_KEY } from '@app/shared/constant';
+import { BACKSPACE_KEY, ESCAPE_KEY, MIDDLE_SNAP_ANGLE, SHIFT_KEY } from '@app/shared/constant';
 import { LineService } from './line-service';
 
 // tslint:disable:no-any
@@ -42,52 +42,70 @@ describe('LineService', () => {
         expect(service).toBeTruthy();
     });
 
-    it(' onMouseClick should set lineStarted to true on mouse click', () => {
+    it(' onMouseClick should set lineStarted to true on first click', () => {
         service.onMouseClick(mouseEvent);
         const expectedResult: Vec2 = { x: 25, y: 25 };
-        service.onMouseClick(mouseEvent);
         expect(service.initialCoord).toEqual(expectedResult);
     });
 
-    it(' onMouseMove should not call drawLine', () => {
+    it(' onMouseClick should push new coord when line is started', () => {
+        service.lineStarted = true;
+        service.onMouseClick(mouseEvent);
+        const expectPathDatalenght = 1;
+        expect(service['pathData'].length).toEqual(expectPathDatalenght);
+    });
+
+    it(' onMouseMove should not call drawLine if line not started', () => {
         service.lineStarted = false;
         service.onMouseMove(mouseEvent);
         expect(drawLineSpy).not.toHaveBeenCalled();
     });
 
-    it(' onMouseMove should call drawLine and shiftDown should be true', () => {
+    it(' onMouseMove should call drawLine when line started and shift pressed', () => {
         service.lineStarted = true;
         service.shiftDown = true;
         service.onMouseMove(mouseEvent);
         expect(drawLineSpy).toHaveBeenCalled();
     });
 
-    it(' onMouseMove should call drawLine and shiftDown should be false', () => {
+    it(' onMouseMove should call drawLine when line started and shift not pressed', () => {
         service.lineStarted = true;
         service.shiftDown = false;
         service.onMouseMove(mouseEvent);
         expect(drawLineSpy).toHaveBeenCalled();
     });
 
-    it('onMouseDoubleClick should call drawnLine', () => {
+    it('onMouseDoubleClick should call drawnLine and endLine', () => {
+        const endLineSpy = spyOn<any>(service, 'endLine').and.callThrough();
         service.lineStarted = true;
         service.onMouseDoubleClick(mouseEvent);
         expect(drawLineSpy).toHaveBeenCalled();
+        expect(endLineSpy).toHaveBeenCalled();
     });
 
-    it('onMouseDoubleClick should not call drawnLine', () => {
+    it('onMouseDoubleClick should not call drawnLine if line not started', () => {
         service.lineStarted = false;
         service.onMouseDoubleClick(mouseEvent);
         expect(drawLineSpy).not.toHaveBeenCalled();
     });
 
-    it('endLine should not draw a line between the first and last point', () => {
+    it('endLine should not draw a line between the first and last point if farther then 20xp', () => {
         service.initialCoord = { x: 0, y: 0 };
         service.currentCoord = { x: 270, y: 270 };
+        const ctxStrokeSpy = spyOn<any>(service['drawingService'].baseCtx, 'stroke').and.callThrough();
         service.endLine();
+        expect(ctxStrokeSpy).not.toHaveBeenCalled();
     });
 
-    it('onKeyUp up Should call drawLine with shiftDown to false when shift is released and mouse is click', () => {
+    it('endLine should draw a line between the first and last point if closer then 20xp', () => {
+        service.initialCoord = { x: 0, y: 0 };
+        service.currentCoord = { x: 15, y: 15 };
+        const ctxStrokeSpy = spyOn<any>(service['drawingService'].baseCtx, 'stroke').and.callThrough();
+        service.endLine();
+        expect(ctxStrokeSpy).toHaveBeenCalled();
+    });
+
+    it('onKeyUp up Should call drawLine when shift is released and line is started', () => {
         service.lineStarted = true;
         const keyEvent = {
             key: SHIFT_KEY,
@@ -97,7 +115,8 @@ describe('LineService', () => {
         expect(drawLineSpy).toHaveBeenCalled();
         expect(service.shiftDown).toBeFalse();
     });
-    it('onKeyUp up Should not call drawLine with  shift key not pressed', () => {
+
+    it('onKeyUp up Should not call drawLine with other key then shift released', () => {
         const keyEvent = {
             key: BACKSPACE_KEY,
         } as KeyboardEvent;
@@ -105,7 +124,8 @@ describe('LineService', () => {
         service.onKeyUp(keyEvent);
         expect(drawLineSpy).not.toHaveBeenCalled();
     });
-    it('onKeyDown up Should call drawLine with shiftDown to true when shift is pressed and mouse is click', () => {
+
+    it('onKeyDown up Should call drawLine with shiftDown to true when shift is pressed and line is started', () => {
         service.lineStarted = true;
         const keyEvent = {
             key: SHIFT_KEY,
@@ -126,10 +146,70 @@ describe('LineService', () => {
         expect(drawLineSpy).not.toHaveBeenCalled();
     });
 
-    it('getSnappedCoord should return point coordinate', () => {
-        service.initialCoord = { x: 18, y: 0 };
-        service.getSnappedCoord();
-        service.initialCoord = { x: 0, y: 18 };
-        service.getSnappedCoord();
+    it('calculateAngle should return the right angle', () => {
+        const firstPoint = { x: 0, y: 0 };
+        const secondPoint = { x: 1, y: 1 };
+        const expectedAngle = MIDDLE_SNAP_ANGLE;
+        expect(service.calculateAngle(firstPoint, secondPoint)).toEqual(expectedAngle);
+    });
+
+    it('getSnappedCoord should return the snapped point coordinate', () => {
+        service.initialCoord = { x: 0, y: 0 };
+        service.currentCoord = { x: 2, y: 0.1 };
+        let expectedPoint = { x: 2, y: 0 };
+        expect(service.getSnappedCoord()).toEqual(expectedPoint);
+
+        service.initialCoord = { x: 0, y: 0 };
+        service.currentCoord = { x: 0.1, y: 2 };
+        expectedPoint = { x: 0, y: 2 };
+        expect(service.getSnappedCoord()).toEqual(expectedPoint);
+
+        service.initialCoord = { x: 0, y: 0 };
+        service.currentCoord = { x: 2, y: 2.1 };
+        expectedPoint = { x: 2, y: 2 };
+        const result = service.getSnappedCoord();
+        expect(result.x).toBeCloseTo(expectedPoint.x, 1);
+        expect(result.y).toBeCloseTo(expectedPoint.y, 1);
+    });
+
+    it('getSnappedCoord should use last path point when not empty', () => {
+        service['pathData'].push({ x: 1, y: 1 });
+        service.currentCoord = { x: 2, y: 1.1 };
+        const expectedPoint = { x: 2, y: 1 };
+        expect(service.getSnappedCoord()).toEqual(expectedPoint);
+    });
+
+    it('OnKeyDown with backspace should remove a point from pathData, and call drawLine if line started', () => {
+        const initialLenght = 3;
+        for (let i = 0; i < initialLenght; i++) {
+            service['pathData'].push({ x: i, y: i });
+        }
+        const event = {
+            key: BACKSPACE_KEY,
+        } as KeyboardEvent;
+
+        service.onKeyDown(event);
+        expect(service['pathData'].length).toEqual(initialLenght - 1);
+        expect(drawLineSpy).not.toHaveBeenCalled();
+        service.lineStarted = true;
+        service.onKeyDown(event);
+        expect(drawLineSpy).toHaveBeenCalled();
+        expect(service['pathData'].length).toEqual(initialLenght - 2);
+    });
+
+    it('OnKeyDown with escape should remove all point from pathData and reset coord', () => {
+        const initialLenght = 3;
+        for (let i = 0; i < initialLenght; i++) {
+            service['pathData'].push({ x: i, y: i });
+        }
+        const event = {
+            key: ESCAPE_KEY,
+        } as KeyboardEvent;
+
+        service.onKeyDown(event);
+        expect(service['pathData'].length).toEqual(0);
+        const resetCoord = { x: 0, y: 0 };
+        expect(service.initialCoord).toEqual(resetCoord);
+        expect(service.currentCoord).toEqual(resetCoord);
     });
 });
--- client/src/app/shared/constant.ts
+++ client/src/app/shared/constant.ts
@@ -3,9 +3,9 @@ export const BACKSPACE_KEY = 'Backspace';
 export const ESCAPE_KEY = 'Escape';
 // disabling magic number to prevent declaring a constant named EIGHT
 // tslint:disable:no-magic-numbers
-export const BASE_MIDDLE_ANGLE_SNAP_DIVIDER = 4;
+export const MIDDLE_ANGLE_SNAP_DIVIDER = 4;
 export const BASE_ANGLE_SNAP_DIVIDER = 8;
-export const MIDDLE_SNAP_ANGLE = Math.PI / BASE_MIDDLE_ANGLE_SNAP_DIVIDER;
+export const MIDDLE_SNAP_ANGLE = Math.PI / MIDDLE_ANGLE_SNAP_DIVIDER;
 export const BASE_SNAP_ANGLE = Math.PI / BASE_ANGLE_SNAP_DIVIDER;
 
 // tslint:enable:no-magic-numbers
