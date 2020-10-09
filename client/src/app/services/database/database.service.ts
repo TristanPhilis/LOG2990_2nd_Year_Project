dd8617daba182798701ee6da3b58ae4b6cba5297
Ramzi <ramzibelbahri@gmail.com>
2020-10-08 20:42:40 -0400

    Added populate method

--- client/src/app/services/database/database.service.ts
+++ client/src/app/services/database/database.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 // import { injectable } from 'inversify';
 import { Collection, FilterQuery, FindOneOptions, MongoClient, MongoClientOptions, UpdateQuery } from 'mongodb';
 import 'reflect-metadata';
+import { DrawingInfo } from '../../classes/drawing-info';
 
 const DB_USER = 'NewUser';
 const DB_PASSWORD = 'eWLZhsX3pGPJJE8N';
@@ -78,6 +79,21 @@ export class DatabaseService {
             .catch((error: Error) => {
                 throw new Error('Failed to delete course');
             });
+    }
+
+    async populateDB(): Promise<void> {
+        const drawings: DrawingInfo[] = [
+            { name: 'Drawing1', tags: ['a', 'c'] },
+            { name: 'Drawing2', tags: ['b', 'c'] },
+            { name: 'Drawing3', tags: ['a'] },
+            { name: 'Drawing4', tags: ['a', 'c', 'd'] },
+            { name: 'Drawing5', tags: ['a', 'b'] },
+        ];
+
+        console.log('THIS ADDS DATA TO THE DATABASE, DO NOT USE OTHERWISE');
+        drawings.forEach((drawing) => {
+            this.addDrawing(drawing);
+        });
     }
 
     constructor() {}
