38c9070a963b8929773002af24599dafb00eb198
TristanPhilis <46928560+TristanPhilis@users.noreply.github.com>
2020-10-11 20:52:23 -0400

    Added test for create-new-drawing-component
    Signed-Off-By: Tristan Philis <tristan.philis@polymtl.ca>

--- client/src/app/components/create-new-drawing/create-new-drawing.component.spec.ts
+++ client/src/app/components/create-new-drawing/create-new-drawing.component.spec.ts
@@ -1,25 +1,37 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+// import { canvasTestHelper } from '@app/classes/canvas-test-helper';
+import { DrawingService } from '@app/services/drawing/drawing.service';
 import { CreateNewDrawingComponent } from './create-new-drawing.component';
 
 describe('CreateNewDrawingComponent', () => {
-  let component: CreateNewDrawingComponent;
-  let fixture: ComponentFixture<CreateNewDrawingComponent>;
+    let component: CreateNewDrawingComponent;
+    let fixture: ComponentFixture<CreateNewDrawingComponent>;
+    let drawServiceSpy: jasmine.SpyObj<DrawingService>;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ CreateNewDrawingComponent ]
-    })
-    .compileComponents();
-  }));
+    beforeEach(async(() => {
+        TestBed.configureTestingModule({
+            declarations: [CreateNewDrawingComponent],
+        }).compileComponents();
+        drawServiceSpy = jasmine.createSpyObj('DrawingService', ['clearCanvas']);
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(CreateNewDrawingComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+        TestBed.configureTestingModule({
+            providers: [{ provide: DrawingService, useValue: drawServiceSpy }],
+        });
+    }));
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    beforeEach(() => {
+        fixture = TestBed.createComponent(CreateNewDrawingComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
+
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
+
+    it('should call clearCanvas when clearCanvas is called', () => {
+        component.clearCanvas();
+        expect(drawServiceSpy.clearCanvas).toHaveBeenCalled();
+        expect(drawServiceSpy.clearCanvas).toHaveBeenCalled();
+    });
 });
