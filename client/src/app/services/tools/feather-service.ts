a7a07dfe7f9d868d3ceab18cd9e60e28f42795dc
Romain Saez <romain.saez@polymtl.ca>
2020-12-05 21:05:25 -0500

    can define the angle of the line

--- client/src/app/services/tools/feather-service.ts
+++ client/src/app/services/tools/feather-service.ts
@@ -5,6 +5,7 @@ import { ToolOption } from '@app/classes/tool-option';
 import { Vec2 } from '@app/classes/vec2';
 import { ColorSelectionService } from '@app/services/color/color-selection-service';
 import { DrawingService } from '@app/services/drawing/drawing.service';
+import { ANGLE_ROTATION, DEFAULT_OPTIONS, DEG_TO_RAD_FACTOR, ROTATION_COMPLETE } from '@app/shared/constant';
 import { DrawingToolId, MouseButton, Options } from '@app/shared/enum';
 
 export const MINIMUM_FEATHER_SIZE = 2;
@@ -13,6 +14,7 @@ export const MINIMUM_FEATHER_SIZE = 2;
 })
 export class FeatherService extends Tool {
     private pathData: Vec2[];
+    mousePosition: Vec2;
 
     constructor(drawingService: DrawingService, colorService: ColorSelectionService) {
         super(drawingService, colorService);
@@ -20,7 +22,10 @@ export class FeatherService extends Tool {
         this.setDefaultOptions();
     }
     setDefaultOptions(): void {
-        const toolOptions = new Map<Options, ToolOption>([[Options.size, { value: MINIMUM_FEATHER_SIZE, displayName: 'Largeur' }]]);
+        const toolOptions = new Map<Options, ToolOption>([
+            [Options.size, { value: MINIMUM_FEATHER_SIZE, displayName: 'Largeur' }],
+            [Options.angle, { value: DEFAULT_OPTIONS.angle, displayName: 'Angle' }],
+        ]);
 
         this.options = {
             primaryColor: this.primaryColor,
@@ -57,15 +62,36 @@ export class FeatherService extends Tool {
     draw(ctx: CanvasRenderingContext2D, drawingAction: DrawingAction): void {
         const path = drawingAction.path;
         const size = drawingAction.options.toolOptions.get(Options.size);
+        const angle = this.options.toolOptions.get(Options.angle);
 
-        if (path && size) {
+        if (path && size && angle) {
             ctx.beginPath();
             ctx.lineWidth = 2;
             ctx.strokeStyle = drawingAction.options.primaryColor.getRgbString();
+            ctx.translate(path[path.length - 1].x, path[path.length - 1].y);
+            ctx.rotate(DEG_TO_RAD_FACTOR * angle.value);
+            ctx.translate(-path[path.length - 1].x, -path[path.length - 1].y);
             ctx.moveTo(path[path.length - 1].x, path[path.length - 1].y);
             ctx.lineTo(path[path.length - 1].x + size.value, path[path.length - 1].y + size.value);
+            ctx.resetTransform();
             ctx.stroke();
         }
+    }
+
+    onWheel(event: WheelEvent): void {
+        const changeAngle = event.altKey ? 1 : 1 * ANGLE_ROTATION;
+        const angle = this.options.toolOptions.get(Options.angle);
+        if (!angle) {
+            return;
+        }
+        let newAngle = angle.value + Math.sign(event.deltaY) * changeAngle;
+        if (newAngle < 0) {
+            newAngle += ROTATION_COMPLETE;
+        } else if (newAngle > ROTATION_COMPLETE) {
+            newAngle -= ROTATION_COMPLETE;
+        }
+        angle.value = newAngle;
+        this.options.toolOptions.set(Options.angle, angle);
     }
 
     getDrawingAction(): DrawingAction {
