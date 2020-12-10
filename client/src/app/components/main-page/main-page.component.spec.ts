9db9caddfb20aa690789b62ce89ccaf31ca29d4c
Romain Saez <romain.saez@polymtl.ca>
2020-12-09 19:51:15 -0500

    fix missing test

--- client/src/app/components/main-page/main-page.component.spec.ts
+++ client/src/app/components/main-page/main-page.component.spec.ts
@@ -1,19 +1,30 @@
 import { HttpClientModule } from '@angular/common/http';
 import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
-import { MatDialogModule } from '@angular/material/dialog';
+import { MatDialog, MatDialogModule } from '@angular/material/dialog';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RouterTestingModule } from '@angular/router/testing';
+import { DrawingService } from '@app/services/drawing/drawing.service';
 import { MainPageComponent } from './main-page.component';
 
+// tslint:disable:no-any
 describe('MainPageComponent', () => {
     let component: MainPageComponent;
     let fixture: ComponentFixture<MainPageComponent>;
+    let drawingServiceSpy: jasmine.SpyObj<DrawingService>;
+    let dialogSpy: jasmine.SpyObj<MatDialog>;
 
     beforeEach(
         waitForAsync(() => {
+            dialogSpy = jasmine.createSpyObj('MatDialog', ['open', 'afterClosed']);
+            drawingServiceSpy = jasmine.createSpyObj('DrawingService', ['clearCanvas', 'sendDrawing', 'fillCanvas']);
+
             TestBed.configureTestingModule({
                 imports: [RouterTestingModule, HttpClientModule, MatDialogModule, BrowserModule, BrowserAnimationsModule],
+                providers: [
+                    { provide: DrawingService, useValue: drawingServiceSpy },
+                    { provide: MatDialog, useValue: dialogSpy },
+                ],
                 declarations: [MainPageComponent],
             }).compileComponents();
         }),
@@ -29,7 +40,24 @@ describe('MainPageComponent', () => {
         expect(component).toBeTruthy();
     });
 
-    it("should have as title 'LOG2990'", () => {
-        expect(component.title).toEqual('LOG2990');
+    it('createNew should have been call', () => {
+        component.createNew();
+        expect(drawingServiceSpy.drawingToLoad).toEqual('');
+    });
+    it('openCarousel should have been call', () => {
+        component.openCarousel();
+        expect(dialogSpy.open).toHaveBeenCalled();
+    });
+
+    it('openGuide should have been call', () => {
+        component.openGuide();
+        expect(dialogSpy.open).toHaveBeenCalled();
+    });
+
+    it('openGuide should have been call', () => {
+        component.continueDrawing();
+        expect(drawingServiceSpy.clearCanvas).toHaveBeenCalled();
+        expect(drawingServiceSpy.fillCanvas).toHaveBeenCalled();
+        expect(drawingServiceSpy.sendDrawing).toHaveBeenCalled();
     });
 });
